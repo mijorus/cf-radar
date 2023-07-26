0ff40a394fe02ce5c7b004f51d161cde2cee37cc
Lorenzo Paderi <39067225+mijorus@users.noreply.github.com>
2023-07-26 09:30:34 +0200

    removed links from footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-    import ky from "ky";
     import "../app.postcss";
     import NavBar from "../NavBar.svelte";
-    import { SERVER_URL } from "$lib/costants";
     import { writable, type Writable } from "svelte/store";
     import { onMount, setContext } from "svelte";
     import "billboard.js/dist/billboard.css";
@@ -25,10 +23,8 @@
 <Footer>
     <FooterCopyright />
     <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-        <FooterLink href="/">About</FooterLink>
-        <FooterLink href="/">Privacy Policy</FooterLink>
-        <!-- <FooterLink href="/">Licensing</FooterLink> -->
-        <!-- <FooterLink href="/">Contact</FooterLink> -->
+        <!-- <FooterLink href="/">About</FooterLink> -->
+        <!-- <FooterLink href="/">Privacy Policy</FooterLink> -->
     </FooterLinkGroup>
 </Footer>
 
